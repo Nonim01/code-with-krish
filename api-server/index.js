f9bacd2422aa8080de1ad3783ebc1d6f057e5dfe
Nonim Dilshan <nonimdilshan876@mail.com>
2025-02-26 18:05:55 +0530

    code formatted

--- api-server/index.js
+++ api-server/index.js
@@ -1,49 +1,50 @@
 const express = require("express");
-const { getMinoraxNumber,getAverageNumber,getSortedNumbers } = require("./util");
+const {
+  getMinoraxNumber,
+  getAverageNumber,
+  getSortedNumbers,
+} = require("./util");
 
 const app = new express();
 const port = 3000;
 
 app.get("/number/min", (req, res) => {
-//can use this function to get the minimum or maxiuum of two numbers in different endpoints
-    const numbers = getMinoraxNumber(req, res);
-    if (!numbers) {
-        return;
-    }
-    const { num1, num2 } = numbers;
-    res.send({ min: Math.min(num1, num2) });
+  //can use this function to get the minimum or maxiuum of two numbers in different endpoints
+  const numbers = getMinoraxNumber(req, res);
+  if (!numbers) {
+    return;
+  }
+  const { num1, num2 } = numbers;
+  res.send({ min: Math.min(num1, num2) });
 });
-
 
 app.get("/number/max", (req, res) => {
   //can use this function to get the minimum or maxiuum of two numbers in different endpoints
-    const numbers = getMinoraxNumber(req, res);
-    if (!numbers) {
-        return;
-    }
-    const { num1, num2 } = numbers;
-    res.send({ max: Math.max(num1, num2) });
+  const numbers = getMinoraxNumber(req, res);
+  if (!numbers) {
+    return;
+  }
+  const { num1, num2 } = numbers;
+  res.send({ max: Math.max(num1, num2) });
 });
 
 //Endpoint for get average of numbers
 app.get("/number/avg", (req, res) => {
   const numbers = getAverageNumber(req, res);
   if (!numbers) {
-      return;
+    return;
   }
   res.send({ avg: numbers });
 });
-
 
 //Endpoint for sorting numbers
 app.get("/number/sort", (req, res) => {
   const sortedNumbers = getSortedNumbers(req, res);
   if (!sortedNumbers) {
-      return;
+    return;
   }
   res.send({ sortedNumbers });
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- api-server/util.js
+++ api-server/util.js
@@ -1,55 +1,84 @@
 //can use this function to get the minimum or maxiuum of two numbers
 function getMinoraxNumber(req, res) {
-    const num1 = parseInt(req.query.num1);
-    const num2 = parseInt(req.query.num2);
+  const num1 = parseInt(req.query.num1);
+  const num2 = parseInt(req.query.num2);
 
-    if (isNaN(num1) || isNaN(num2)) {
-        return res.status(400).send({ error: "Both num1 and num2 should be valid numbers" });  
-    }
+  if (isNaN(num1) || isNaN(num2)) {
+    return res
+      .status(400)
+      .send({ error: "Both num1 and num2 should be valid numbers" });
+  }
 
-    const min = Math.min(num1, num2);
-    res.status(200).send({ min });
+  const min = Math.min(num1, num2);
+  res.status(200).send({ min });
 
-    return { num1, num2 };
+  return { num1, num2 };
 }
 
 function getAverageNumber(req, res) {
-     // Getting the numbers from the query string and turn them into an array of numbers
-    const numbers = req.query.numbers.split(',').map(Number);
+  // Getting the numbers from the query string and turn them into an array of numbers
+  const numbers = req.query.numbers.split(",").map(Number);
 
-    if (numbers.some(isNaN)) {
-        return res.status(400).send({ error: "All values should be valid numbers" });
-    }
+  if (numbers.some(isNaN)) {
+    return res
+      .status(400)
+      .send({ error: "All values should be valid numbers" });
+  }
   // Add up all the numbers in to the array
-    const sum = numbers.reduce((acc, num) => acc + num, 0);
-    const avg = sum / numbers.length;
-    res.status(200).send({ avg });
+  const sum = numbers.reduce((acc, num) => acc + num, 0);
+  const avg = sum / numbers.length;
+  res.status(200).send({ avg });
 
-    return numbers;
+  return numbers;
 }
 
 function getSortedNumbers(req, res) {
-    // getting numbers from the query string, split them into an array and convert to numbers
-    const numbers = req.query.numbers.split(',').map(Number);
-    //getting the type
-    const type = req.query.type;
+  // getting numbers from the query string, split them into an array and convert to numbers
+  const numbers = req.query.numbers.split(",").map(Number);
+  //getting the type
+  const type = req.query.type;
 
-    if (numbers.some(isNaN)) {
-        return res.status(400).send({ error: "All values should be valid numbers" });
-    }
+  if (numbers.some(isNaN)) {
+    return res
+      .status(400)
+      .send({ error: "All values should be valid numbers" });
+  }
 
-    if (type !== 'asc' && type !== 'dec') {
-        return res.status(400).send({ error: "Type must be either 'asc' or 'dec'" });
-    }
+  if (type !== "asc" && type !== "dec") {
+    return res
+      .status(400)
+      .send({ error: "Type must be either 'asc' or 'dec'" });
+  }
 
-    // sort the numbers based on the 'type' parameter
-    // If 'asc' sort in ascending order S to L
-    // If 'dec' sort in descending order L to S
-    const sortedNumbers = numbers.sort((a, b) => type === 'asc' ? a - b : b - a);
-    res.status(200).send({ sortedNumbers });
+  // sort the numbers based on the 'type' parameter
+  // If 'asc' sort in ascending order S to L
+  // If 'dec' sort in descending order L to S
+  const sortedNumbers = numbers.sort((a, b) =>
+    type === "asc" ? a - b : b - a
+  );
+  res.status(200).send({ sortedNumbers });
 
-    return sortedNumbers;
+  return sortedNumbers;
 }
 
+function getCountOccurrences(req, res) {
+  const values = req.query.numbers.split(",");
+  const searchValue = req.query.search;
 
-module.exports = { getMinoraxNumber, getAverageNumber, getSortedNumbers, getSortedNumbers };
+  if (!searchValue) {
+    return res.status(400).send({ error: "Search value must be provided" });
+  }
+
+  const count = values.filter((value) => value === searchValue).length;
+  res.status(200).send({ count });
+
+  return count;
+}
+
+module.exports = {
+  getMinoraxNumber,
+  getAverageNumber,
+  getSortedNumbers,
+  getSortedNumbers,
+  getCountOccurrences,
+};
